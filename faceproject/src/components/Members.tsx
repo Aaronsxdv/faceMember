b0502e040038dca919270ba7a344e0ff0bb4d52a
Bohdan Myrinets <myrinets.bohdan@gmail.com>
2022-05-05 01:55:19 +0200

    Pre-final commit
    Left: button to change current added member based on radio input
    Visual changes

--- faceproject/src/components/Members.tsx
+++ faceproject/src/components/Members.tsx
@@ -1,8 +1,8 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import SearchBar from './SearchBar';
 import TopBar from './TopBar';
 import styled from 'styled-components';
-
+import User from './User';
 const StyledMembers = {
     MemberInput : styled.input`
         align-self: center;
@@ -23,6 +23,7 @@ const StyledMembers = {
         }
     `,
     Container : styled.div`
+        width: 100%;
         display:flex;
         flex-direction:column;
         text-align:center;
@@ -30,6 +31,21 @@ const StyledMembers = {
     `,
     CurrentImage : styled.img`
         align-self: center;
+    `,
+    MemberTable : styled.table`
+        table-layout: fixed ;
+        margin: 0 auto;
+        width: 40% ;
+        column-gap: 9px;
+        font-family: Montserrat,sans-serif;
+        border-collapse: collapse;
+    `,
+    TableDiv : styled.div`
+        text-align:center;
+        justify-content: center;
+    `,
+    Th : styled.th`
+        border-bottom: 1px solid rgba(255, 255, 255, .3);
     `
 }
 
@@ -39,48 +55,103 @@ export async function getMembers(url : string) {
     return response.json();
 }
  
-function handleSearchChange(){
-    console.log("hello world")
+
+interface MemberType {
+    imgLink: string;
+    watchlist: string;
+    fullName: string;
 }
 
 const Members : React.FC = () => {
-  const [member,setMember] = useState(0);  
 
+    const [currentMember,setCurrentMember] = useState({
+        imgLink : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/1280px-A_black_image.jpg", 
+        fullName : "Custom Member", 
+        watchlist : "No List"
+    });  
+    const [searchField,setSearchField] = useState("")
+    const [members,setMembers] = useState<{imgLink: string,watchlist: string, fullName: string}[]>([])
+    const [membersSearched,setMembersSearched] = useState<{imgLink: string,watchlist: string, fullName: string}[]>([])
+    const [membersDynamic,setMembersDynamic] = useState<JSX.Element[] | null>(null)
+    
+    function updateInput(input : string) {
+        const filtered = members.filter(member => {
+         return member.fullName.toLowerCase().includes(input.toLowerCase())
+        })
+        setMembersSearched(filtered);
+    }   //search function fullname
 
-  return (
-    <StyledMembers.Container>
-        <TopBar/>
-        <StyledMembers.CurrentImage width="20%" height="20%" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/1280px-A_black_image.jpg"/>
-        <StyledMembers.MemberInput type="text" id="member-search" onChange={handleSearchChange} name="s"/>
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th>   </th>
-                    <th>Full Name</th>
-                    <th>Watchlist</th>
-                </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>
-                    <input type="radio" name="radios" id="radio1"/>
-                    <label htmlFor="radio1">Label 1</label>
-                </td>
-                <td>Regular</td>
-                <td>IrRegular</td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="radio" name="radios" id="radio2"/>
-                    <label htmlFor="radio2">Label 2</label>
-                </td>
-                <td>Regular</td>
-                <td>IrRegular</td>
-            </tr>
-            </tbody>
-        </table>
-    </StyledMembers.Container>
-  );
+    function memberList(membersToTransform: {imgLink: string,watchlist: string, fullName: string}[]){
+        let temp: JSX.Element[] = [];
+        membersToTransform.forEach((member) => {
+            temp.push(<User imgLink={member.imgLink} watchlist={member.watchlist} fullName={member.fullName} />)
+        })
+        console.log("HERE")
+        setMembersDynamic(temp);
+    }
+
+    function handleAddMember(){
+        //let x = document.querySelector('input:checked').value;
+    }
+
+    useEffect(() => {
+        const memberListInit = async() => {
+            let memberJSONList = [];
+            try{
+                var data = await getMembers("http://localhost:4000/members");
+                let numResults = data.length;
+                for(let i=0;i<numResults;i++){
+                  let cMember = {
+                    imgLink: data[i].imageURL,
+                    fullName: data[i].fullName,
+                    watchlist: data[i].watchlistName,
+                    watchlistId : data[i].watchlistId
+                  };
+                  memberJSONList.push(cMember); 
+                }
+                setMembers(memberJSONList);
+            }catch (err){
+                console.log(err);
+            }
+            memberList(members);
+        }
+        
+        memberListInit();
+    },[]) //useEffect used on initial render which inits the members from the server
+
+    useEffect(() => {
+        if(searchField == ""){
+            memberList(members)
+        }
+        else{
+            updateInput(searchField)
+            memberList(membersSearched)
+        }
+    }) //useEffect used every re-render
+  
+    return (
+        <StyledMembers.Container>
+            <TopBar/>
+            <StyledMembers.CurrentImage width="20%" height="20%" src={currentMember.imgLink}/>
+            <StyledMembers.MemberInput type="text" id="member-search" onChange={(e) => setSearchField(e.target.value)} name="s"/>
+            <StyledMembers.TableDiv>
+                <StyledMembers.MemberTable className="table table-hover">
+                    <thead>
+                        <tr>
+                            <StyledMembers.Th>   </StyledMembers.Th>
+                            
+                            <StyledMembers.Th>Full Name</StyledMembers.Th>
+                            <StyledMembers.Th>Watchlist</StyledMembers.Th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {membersDynamic}
+                    </tbody>
+                </StyledMembers.MemberTable>
+            </StyledMembers.TableDiv>
+            <button onClick={handleAddMember}>Add to member</button>
+        </StyledMembers.Container>
+    );
 }
 ;
 export default Members;
--- faceproject/src/components/User.tsx
+++ faceproject/src/components/User.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+interface UserProps {
+  imgLink: string;
+  watchlist: string;
+  fullName: string;
+}
 
-const User : React.FC = () => {
+
+const StyledUser = {
+  Tr : styled.tr`
+  `,
+  MemberInput : styled.input`
+    
+  `,
+  Image : styled.img`
+    width:30%;
+    height:30%;
+  `,
+  Td : styled.td`
+    font-family: Montserrat,sans-serif;
+    border-bottom: 1px solid rgba(255, 255, 255, .3);
+  
+  `
+}
+function User(props:UserProps){
   return (
-    <div className="App">
-      <h1>Hello UserName</h1>
-
-    </div>
+      <StyledUser.Tr>
+        <StyledUser.Td>
+          <StyledUser.MemberInput type="radio" name="radios" id="radio1"/>
+          <label htmlFor="radio1"><StyledUser.Image src={props.imgLink} alt="Link is broken:(" /></label>
+        </StyledUser.Td>
+        <StyledUser.Td>{props.fullName}</StyledUser.Td>
+        <StyledUser.Td>{props.watchlist}</StyledUser.Td>
+      </StyledUser.Tr>
   );
 }
-;
+
 export default User;
